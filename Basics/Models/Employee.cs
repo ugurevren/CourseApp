ce429fe3ebf6ad535b650d6743ddcd7ae82ab74d
Uğur Evren Çamalan <ugurevrencamalan@gmail.com>
2024-07-26 02:05:55 +0300

    Basic trials

---
+++ Basics/Models/Employee.cs
@@ -0,0 +1,9 @@
+namespace Basics.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "John Doe";
+        public string Department { get; set; } = "IT";
+    }
+}
